047783fbf52e69bd7ad02797f74b021599546961
abdulrahmanahmed291 <abdulrahman.ahmed@redbullmobile.sa>
2024-11-16 01:46:20 +0200

    remove unused imports.

--- app/src/main/java/com/abdelrahman/marvelapp/MainActivity.kt
+++ app/src/main/java/com/abdelrahman/marvelapp/MainActivity.kt
@@ -12,31 +12,18 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.abdelrahman.feature_characters_presentation.characterslist.ui.CharactersScreen
-import com.abdelrahman.feature_characters_presentation.characterslist.viewmodel.CharactersListContract
 import com.abdelrahman.feature_characters_presentation.characterslist.viewmodel.CharactersViewModel
 import com.abdelrahman.marvelapp.navigation.CharactersScreen
 import com.abdelrahman.marvelapp.ui.theme.MarvelAppTheme
-import com.abdelrahman.shared_presentation.LoadingTypes
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.filter
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
